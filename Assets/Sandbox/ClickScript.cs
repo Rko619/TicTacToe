43cff08c8ae472f94c1960218bbb0d2ecfe2cbb0
Raghul95 <praghul1995@gmail.com>
2017-11-24 18:31:32 +0530

    Added Something

---
+++ Assets/Sandbox/ClickScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour {
+
+    public bool isX;
+
+    public void OnClick()
+    {
+        Image m = GetComponent<Image>();
+        if (TicTacToeScript.instance.isPlayer1)
+        {
+            m.sprite = TicTacToeScript.instance.x;
+            TicTacToeScript.instance.OnClick();
+            isX = true;
+        }
+        else
+        {
+            m.sprite = TicTacToeScript.instance.o;
+            TicTacToeScript.instance.OnClick();
+            isX = false;
+        }
+
+    }
+}
---
+++ Assets/Sandbox/TicTacToeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicTacToeScript : MonoBehaviour
+{
+    public static TicTacToeScript instance;
+    public Sprite x, o;
+    public bool isPlayer1=true;
+    public bool[,] customMatrix=new bool[3][3];
+
+
+    void OnEnable()
+    {
+        instance = this;
+    }
+
+
+    public void OnClick()
+    {
+        if (isPlayer1)
+        {
+            isPlayer1 = false;
+
+        }
+        else
+        {
+            isPlayer1 = true;
+        }
+    }
+}
+
